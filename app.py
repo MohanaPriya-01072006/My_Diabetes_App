d27eaad1467874a093bad95f5ae9d6a2ca88cb96
MohanaPriya-01072006 <23ucs119@kamarajengg.edu.in>
2025-06-18 19:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if st.button("Predict"):
     st.subheader("Prediction Result:")
     st.write(f"Predicted probability of having diabetes: *{prediction:.2f}*")
 k
-    if prediction >0.5 1:
+    if prediction >0.5:
         st.error("Warning: High chance of diabetes")
     else:
         st.success("Low chance of diabetes")
